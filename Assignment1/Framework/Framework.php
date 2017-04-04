82d14800bb063a81bdd80cbf3212c8eae6a2a417
Simon Bothen <caresilabs@gmail.com>
2017-04-04 16:19:48 +0000

    Test Feature

--- Assignment1/Framework/Framework.php
+++ Assignment1/Framework/Framework.php
@@ -1,10 +1,10 @@
 <?php
 
-$name = "Index";    //ucfirst(basename($_SERVER['PHP_SELF'], '.php'));
+$name = "Index";   //ucfirst(basename($_SERVER['PHP_SELF'], '.php')); //__FILE__
 if (isset($_GET['controller']) && !empty($_GET['controller'])) {
     $name = $_GET['controller'];
 }
-$controllerName = $name  . "Controller"; //__FILE__
+$controllerName = $name  . "Controller"; 
 
 require_once("Controllers/" . $controllerName . '.php');
 

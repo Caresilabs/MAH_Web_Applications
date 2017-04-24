632c41321fbc4426fc41e8df2df4884e645ff855
Simon Bothen <caresilabs@gmail.com>
2017-04-24 16:17:19 +0000

    Added assignment2. Not including testing

---
+++ Assignment2/app/Http/Controllers/ProductsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductsController extends Controller
+{
+    
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+    
+    public function show($id)
+    {
+        $product = Product::with('reviews', 'stores')->find($id);
+        return response()->json($product);
+    }
+    
+    public function create(Request $request)
+    {
+        $product = new Product;
+        $product->title = $request->title;
+        $product->brand = $request->brand;
+        $product->price = $request->price;
+        $product->image = $request->image;
+        $product->description = $request->description;
+        
+        $product->save();
+
+        foreach ($request->get("stores") as $store) {
+            $product->stores()->attach($store);
+        }
+        
+          return response()->json(["success" => true]);
+    }
+}
---
+++ Assignment2/app/Http/Controllers/ReviewsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use App\Review;
+
+class ReviewsController extends Controller
+{
+   
+    public function index()
+    {
+        $reviews = Review::all();
+        return response()->json($reviews);
+    }
+
+}

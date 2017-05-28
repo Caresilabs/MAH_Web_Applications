4099913601cfda9c98d7cadf8302e8d528572840
Patrik Nilsson <patriknilsson1337@live.se>
2017-05-28 11:01:16 +0200

    final test

--- Assignment4/resources/views/blogpostDetails.blade.php
+++ Assignment4/resources/views/blogpostDetails.blade.php
@@ -3,7 +3,7 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <div class="panel panel-default">
-        <div class="panel-heading">Dev Post</div>
+        <div class="panel-heading">Post</div>
 
         <div class="panel-body">
 
